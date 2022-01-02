d0b7143244b78840e1c3f1b16d453218054e2a77
harisli <harisli@tencent.com>
2022-01-02 16:32:24 +0800

    first commit

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -6,8 +6,8 @@ module.exports = [
   { text: '翻译', link: '/translation/' },
   {
     text: '个人主页', items: [
-      { text: 'github', link: 'https://github.com/WangYuLue' },
-      { text: '知乎', link: 'https://zhihu.com/people/wang-yu-lue' }
+      { text: 'github', link: '' },
+      { text: '知乎', link: '' }
     ]
   },
 ]
